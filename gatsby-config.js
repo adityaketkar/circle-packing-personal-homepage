91d0384e865384335cf5e4ae760b340ff5278a21
Aditya Ketkar <ketkaraditya615@gmail.com>
2020-06-03 15:28:51 +0530

    Initial commit from gatsby: (https://github.com/codebushi/gatsby-starter-dimension.git)

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter - Dimension V2',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+  ],
+}
