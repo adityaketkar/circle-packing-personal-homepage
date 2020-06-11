cbb1559d338d33e8c44fa6d98933fbc68ab6153a
Aditya Ketkar <ketkaraditya615@gmail.com>
2020-06-11 20:48:24 +0530

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist.... Wanna head back <a href="https://www.adityaketkar.me">Home</a> ?</p>
+    <p>[ If you're here for Alexa Companion, I had to take it down due to some issues. Contact me <a href="https://www.adityaketkar.me">here</a> to get it offline. ]</p> 
+    <hr></hr>
+    
+    <h2>Don't be sad, it's CATURDAY!</h2>
+    <p>Here is a <strong>truly</strong> random cat for you :)</p> 
+    <img src="https://theoldreader.com/kittens/600/400"></img>
   </Layout>
 )
 
